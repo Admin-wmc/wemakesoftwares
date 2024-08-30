47285cea21f74ba17e67c758cc2c403d76cfed80
Admin-wmc <wemakesoftwaresofficial@gmail.com>
2024-08-30 18:16:45 +0530

    udpated routes and fixed img paths

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,19 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('', views.home,name="home"),
+    path('about', views.about,name="about"),
+    path('services', views.services,name="services"),
+    path('service_details', views.service_details,name="service_details"),
+    path('pricing_plan', views.pricing_plan,name="pricing_plan"),
+    path('contact', views.contact,name="contact"),
+    path('study_grid', views.study_grid,name="study_grid"),
+    path('study_details', views.study_details,name="study_details"),
+    path('team_grid', views.team_grid,name="team_grid"),
+    path('team_details', views.team_details,name="team_details"),
+    path('faq', views.faq,name="faq"),
+    path('blog_grid', views.blog_grid,name="blog_grid"),
+    path('blog_list', views.blog_list,name="blog_list"),
+    path('blog_details', views.blog_details,name="blog_details"),
+    path('error_404', views.error_404,name="error_404"),
 
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,33 @@ from django.shortcuts import render
 
 def home(requests):
     return render(requests,"index.html")
+
+def about(requests):
+    return render(requests,"about.html")
+
+def services(requests):
+    return render(requests,"services.html")
+def service_details(requests):
+    return render(requests,"service_details.html")
+def pricing_plan(requests):
+    return render(requests,"pricing_plan.html")
+def contact(requests):
+    return render(requests,"contact.html")
+def study_grid(requests):
+    return render(requests,"study_grid.html")
+def study_details(requests):
+    return render(requests,"study_details.html")
+def team_grid(requests):
+    return render(requests,"team_grid.html")
+def team_details(requests):
+    return render(requests,"team_details.html")
+def faq(requests):
+    return render(requests,"faq.html")
+def error_404(requests):
+    return render(requests,"404.html")
+def blog_grid(requests):
+    return render(requests,"blog_grid.html")
+def blog_list(requests):
+    return render(requests,"blog_list.html")
+def blog_details(requests):
+    return render(requests,"blog_details.html")
